f780eb6ae0b5499e10df7d31f12b40523ea611ad
codiedigital <contato@codie.digital>
2025-08-20 14:03:13 -0300

    dd

--- src/app/api/send-reminders/route.ts
+++ src/app/api/send-reminders/route.ts
@@ -13,9 +13,8 @@ export async function GET(request: NextRequest) {
     console.log("Eu estou executando");
 
     const response = await fetch(process.env.apiLocal + "send-reminders");
-    const data = await response.json();
 
-    return NextResponse.json({ success: true, data }, { status: 200 });
+    return NextResponse.json({ success: true }, { status: 200 });
   } catch (err: any) {
     return NextResponse.json(
       { success: false, error: err.message },
